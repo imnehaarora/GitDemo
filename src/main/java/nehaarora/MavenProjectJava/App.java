c32fa7fcf9c902ea88443ca3bbc63642dcd01e81
imnehaarora <nehaarora.cse17@jecrc.ac.in>
2022-02-08 18:08:42 +0530

    first commit

---
+++ src/main/java/nehaarora/MavenProjectJava/App.java
@@ -0,0 +1,13 @@
+package nehaarora.MavenProjectJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
